9c56d3705b1b1b1cd7d668c6becb4ad962210fb5
undrown.off@gmail.com <undrown.off@gmail.com>
2020-06-10 15:23:39 +0300

    added async request

--- src/DatabaseConnector.java
+++ src/DatabaseConnector.java
@@ -1,17 +1,47 @@
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DatabaseConnector {
-    private final String webApi = "https://anmedia-server.000webhostapp.com/include/api.php?";
+    private final String webApi = "https://anmedia-server.000webhostapp.com/include/api.php";
     private String uid;
+    private static final HttpClient httpClient = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_2)
+            .connectTimeout(Duration.ofSeconds(10))
+            .build();
 
     public DatabaseConnector(String user_id){
         uid = user_id;
+    }
+
+    public DatabaseConnector(Long phone_num){
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(webApi))
+                .timeout(Duration.ofSeconds(10))
+                .build();
+    }
+
+    /**
+     *  Get user id from server by phone
+     * @return JSON-string of user data
+     */
+    private String performLoginRequest(Long phone_num){
+        String request =webApi+"action=login&phone="+phone_num.toString();
+        StringBuilder result = new StringBuilder();
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new URL(request).openStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (sc.hasNext()) result.append(sc.nextLine());
+        return result.toString();
     }
 
     /**
@@ -50,11 +80,28 @@ public class DatabaseConnector {
         return true;
     }
 
-    public void performAsyncPush(ArrayList<Entry> data){
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(webApi))
-                .timeout(Duration.ofSeconds(5))
-                .header("Content-Type", "application/json")
-                .build();
+    /**
+     * Push data to server
+     * @param data ArrayList of Entries
+     * @return true if all queries successed
+     * @throws InterruptedException
+     */
+    public boolean performAsyncPush(ArrayList<Entry> data) throws InterruptedException {
+        data.forEach(entry -> {
+            String url = webApi + "action=update_data&time_add=" + entry.timeAdd +
+                    "&time_start=" + entry.timeStart +
+                    "&time_end=" + entry.timeEnd;
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .timeout(Duration.ofSeconds(5))
+                    .header("Content-Type", "application/json")
+                    .build();
+            httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .thenAccept(System.out::println);
+        });
+        Thread.sleep(2000);
+        System.out.println("END");
+        return true;
     }
 }
